82f85994cffcee774bcd7488c6266436d4382b1d
Fabiola Rodriguez <alejandrafabiolalopez58@gmail.com>
2022-06-18 18:38:58 -0500

    Video 3.4

--- conversor.py
+++ conversor.py
@@ -1,3 +1,12 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuantos pesos" + "tipo_pesos" + "tiene?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+
 menu = """
 Bienvenido al conversor de monedas 
 
@@ -10,28 +19,10 @@ Elige una opción: """
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input("¿Cuantos pesos mexicanos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 20.34
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("mexicanos", 20.34)
 elif opcion == 2:
-    pesos = input("¿Cuantos pesos colombianos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 3905.90
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("colombianos", 3905.90)
 elif opcion == 3:
-    pesos = input("¿Cuantos pesos argentinos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 123.16
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("argentinos", 123.16)
 else:
     print('Ingresa una opción correcta por favor')
--- funciones.py
+++ funciones.py
@@ -8,21 +8,28 @@
 # imprimir_mensaje()
 
 
-def conversasion(mensaje):
-    print('Hola')
-    print('Cómo estás?')
-    print(mensaje)
-    print('Adios')
+# def conversasion(mensaje):
+#     print('Hola')
+#     print('Cómo estás?')
+#     print(mensaje)
+#     print('Adios')
 
 
-opcion = int(input('Elige una opción(1, 2, 3): '))
-if opcion == 1:
-    conversasion('Elegiste la opción 1')
-elif opcion == 2:
-    conversasion('Elegiste la opción 2')
-elif opcion == 3:
-    conversasion('Elegiste la opción 3')
-else:
-    print('Escribe la opción correcta')
+# opcion = int(input('Elige una opción(1, 2, 3): '))
+# if opcion == 1:
+#     conversasion('Elegiste la opción 1')
+# elif opcion == 2:
+#     conversasion('Elegiste la opción 2')
+# elif opcion == 3:
+#     conversasion('Elegiste la opción 3')
+# else:
+#     print('Escribe la opción correcta')
 
+def suma(a, b):
+    print('Se suman dos números')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
 
